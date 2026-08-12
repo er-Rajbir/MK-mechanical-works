40952d859fe98d1bd396949b3b6cc6630964bbe5
er-Rajbir <anandrajbir13@gmail.com>
2026-08-12 13:13:43 +0530

    allowed host

--- config/config/settings.py
+++ config/config/settings.py
@@ -33,8 +33,7 @@ ALLOWED_HOSTS = [
     host.strip()
     for host in os.environ.get(
         "ALLOWED_HOSTS",
-        "127.0.0.1,localhost",
-        "mk-mechanical-worksconfig-production.up.railway.app"
+        "127.0.0.1,localhost"
     ).split(",")
     if host.strip()
 ]
